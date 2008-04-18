a7c5026bcfc80f63177275fe65dca785e3f75ca1
Andrew Dupont <prototype@andrewdupont.net>
2008-04-18 02:56:28 -0500

    Fix issue where IE fails to GC when the page is navigated away from before it's done loading. [#25 state:resolved]

--- src/event.js
+++ src/event.js
@@ -203,17 +203,33 @@ Object.extend(Event, (function() {
         cache[id][eventName] = null;
   }
   
+  function onStop() {
+    document.detachEvent('onstop', onStop);
+    destroyCache();
+  }
   
-  // Internet Explorer needs to remove event handlers on page unload
-  // in order to avoid memory leaks.
+  function onBeforeUnload() {
+    if (document.readyState === "interactive") {
+      document.attachEvent('onstop', onStop);
+      (function() { document.detachEvent('onstop', onStop); }).defer();
+    }
+  }
+  
   if (window.attachEvent) {
+    // Internet Explorer needs to remove event handlers on page unload
+    // in order to avoid memory leaks.
     window.attachEvent("onunload", destroyCache);
+
+    // IE also doesn't fire the unload event if the page is navigated away
+    // from before it's done loading. Workaround adapted from
+    // http://blog.moxiecode.com/2008/04/08/unload-event-never-fires-in-ie/.
+    window.attachEvent("onbeforeunload", onBeforeUnload);        
   }
   
   // Safari has a dummy event handler on page unload so that it won't
   // use its bfcache. Safari <= 3.1 has an issue with restoring the "document"
   // object when page is returned to via the back button using its bfcache.
-  if (Prototype.Browser.WebKit) {    
+  if (Prototype.Browser.WebKit) {
     window.addEventListener('unload', Prototype.emptyFunction, false);
   }
     
